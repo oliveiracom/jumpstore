2be1a478721fe9b5632077f0620e7eb14933da46
oliveiracom <caroline@oliveiracomunicacao.com.br>
2020-06-02 17:26:54 -0300

    edit serve-static

--- backend/app.js
+++ backend/app.js
@@ -4,16 +4,25 @@ var serveStatic = require('serve-static')
 // Serve up mock-api folder
 app.use('/api', serveStatic('api', {
   'index': false,
-  'setHeaders': setJsonHeaders
+  //'setHeaders': setJsonHeaders
+  'setHeaders': {
+    'Content-type' : 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  }
 }))
  
 // Set header to force download
 function setJsonHeaders (res, path) {
-  res.setHeader('Content-type', 'application/json')
+  res.setHeader('Content-type', 'application/json'),
+  res.setHeader('Content-Disposition', contentDisposition(path))
+}
+
+function setCors (res, path) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
 }
 
 // Serve up public folder
-app.use('/', serveStatic('public', {'index': ['index.html', 'index.htm']}))
+//app.use('/', serveStatic('public', {'index': ['index.html', 'index.htm']}))
 
 app.listen(8888, function() {
     console.log('Acesse: http://localhost:8888')
